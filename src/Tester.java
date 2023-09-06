35bc9e35bd3ef0acafde29eb0d7b12d6a2b7613b
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-09-06 17:32:55 +0200

    Update Tester.java

--- src/Tester.java
+++ src/Tester.java
@@ -1,9 +1,23 @@
 public class Tester {
+    public static void main(String[] args) {
+        // Crea due istanze di Employee
+        Employee employee1 = new Employee("John Doe", 1001);
+        Employee employee2 = new Employee("Jane Smith", 1002);
 
-   static Employee employeenew1 = new Employee("gioacomo", "verdi", "Via dante 456");
-   static Employee employeenew2 = new Employee("luca", "neri", "Via crispi 234");
+        // Crea due istanze di Badge
+        Badge badge1 = new Badge(1, "Livello 1");
+        Badge badge2 = new Badge(2, "Livello 2");
 
+        // Assegna i badge ai dipendenti
+        employee1.assignBadge(badge1);
+        employee2.assignBadge(badge2);
 
-
+        // Mostra i dettagli dei badge dei dipendenti
+        System.out.println("Dettagli dei badge dei dipendenti:");
+        System.out.println("Employee 1: " + employee1.getName() + ", ID: " + employee1.getEmployeeId());
+        System.out.println("ID Badge: " + employee1.getBadge().getBadgeId() + ", Livello di Accesso: " + employee1.getBadge().getAccessLevel());
+        System.out.println();
+        System.out.println("Employee 2: " + employee2.getName() + ", ID: " + employee2.getEmployeeId());
+        System.out.println("ID Badge: " + employee2.getBadge().getBadgeId() + ", Livello di Accesso: " + employee2.getBadge().getLivelloAccesso());
     }
-
+}
